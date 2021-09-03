172cb55d364f4bbf618312fa6a5ed8e82855083f
inas hyatt <inashyatt@inass-mbp-2.attlocal.net>
2021-09-02 17:17:09 -0700

    added code for median stream

---
+++ fourteen_patterns/two_heaps/find_median_number_stream/find_median_number_stream.py
@@ -0,0 +1,25 @@
+import heapq
+
+class MedianOfAStream:
+  def __init__(self):
+    self.max_heap = []
+    self.min_heap = []
+
+    heapq.heapify(self.max_heap)
+    heapq.heapify(self.min_heap)
+
+  def insert_num(self, num):
+    heapq.heappush(self.max_heap, -num)
+
+    heapq.heappush(self.min_heap, -self.max_heap[0])
+    heapq.heappop(self.max_heap)
+
+    if len(self.min_heap) > len(self.max_heap):
+      heapq.heappush(self.max_heap, -self.min_heap[0])
+      heapq.heappop(self.min_heap)
+
+  def find_median(self):
+    if len(self.max_heap) == len(self.min_heap):
+        return (-self.max_heap[0] + self.min_heap[0]) / 2
+    else:
+        return -self.max_heap[0]
